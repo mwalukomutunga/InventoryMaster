5fba7698881542d26ccd8bb055fb1b4fa6258448
steve <mwalukomutunga@gmail.com>
2021-01-07 15:36:58 +0300

    added wpf client

---
+++ POS.Client/DependencyContainer.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace POS.Client
+{
+    public class DependencyContainer
+    {
+        public static ILifetimeScope Container { get; set; }
+        static DependencyContainer()
+        {
+            if (Container == null) Container = BuildContainer();
+        }
+
+        private static IContainer BuildContainer()
+        {
+            var builder = new ContainerBuilder();
+           
+            // Scan an assembly for components
+            var assembly = Assembly.GetExecutingAssembly();
+            builder.RegisterAssemblyTypes(assembly)
+                   .Where(t => t.Name.EndsWith("ViewModel"))
+                   .AsSelf();
+
+            var container = builder.Build();
+            return container;
+        }
+
+    }
+}
---
+++ POS.Client/ViewModels/MainViewModel.cs
@@ -0,0 +1,80 @@
+﻿using Autofac;
+using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POS.Client.ViewModels
+{
+    public enum DocumentType { Items, Customers, Category, Users, Inventory, Orders, Brands, Reports, Sales, Transactions, UOM, InventoryAdjustment, Company, CostCenter, Payments, PayMethods }
+    public class MainViewModel : ViewModelBase
+    {
+        protected IWindowService WindowService { get { return this.GetService<IWindowService>(); } }
+        public DelegateCommand<DocumentType> NavCommand { get; private set; }
+        public ILifetimeScope Container { get; private set; }
+        public MainViewModel()
+        {
+            NavCommand = new DelegateCommand<DocumentType>(OpenPage);
+            Container = DependencyContainer.Container;
+        }
+
+        private void OpenPage(DocumentType param)
+        {
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                switch (param)
+                {
+                    //case DocumentType.Items:
+                    //    WindowService.Show("Item", scope.Resolve<ItemViewModel>());
+                    //    break;
+                    //case DocumentType.Customers:
+                    //    WindowService.Show("Customer", scope.Resolve<CustomerViewModel>());
+                    //    break;
+                    //case DocumentType.Category:
+                    //    WindowService.Show("Category", scope.Resolve<CategoryViewModel>());
+                    //    break;
+                    //case DocumentType.Users:
+                    //    WindowService.Show("Staff", scope.Resolve<StaffViewModel>());
+                    //    break;
+                    //case DocumentType.Inventory:
+                    //    WindowService.Show("ItemInventory", scope.Resolve<ItemInventoryViewModel>());
+                    //    break;
+                    //case DocumentType.Orders:
+                    //    break;
+                    //case DocumentType.Brands:
+                    //    WindowService.Show("Brand", scope.Resolve<BrandViewModel>());
+                    //    break;
+                    //case DocumentType.CostCenter:
+                    //    WindowService.Show("SalesOutlet", scope.Resolve<SalesOutletViewModel>());
+                    //    break;
+                    //case DocumentType.Sales:
+                    //    WindowService.Show("Sales", scope.Resolve<SalesViewModel>());
+                    //    WindowService.Title = "Sales";
+                    //    WindowService.WindowState = DXWindowState.Maximized;
+                    //    break;
+                    //case DocumentType.Transactions:
+                    //    break;
+                    //case DocumentType.UOM:
+                    //    WindowService.Show("UnitOfMeasurement", scope.Resolve<UnitOfMeasurementViewModel>());
+                    //    break;
+                    //case DocumentType.InventoryAdjustment:
+                    //    WindowService.Show("InventoryAdjustment", scope.Resolve<AdjustmentViewModel>());
+                    //    break;
+                    //case DocumentType.Company:
+                    //    WindowService.Show("Company", scope.Resolve<CompanyViewModel>());
+                    //    break;
+                    //case DocumentType.Payments:
+                    //    WindowService.Show("Payments", scope.Resolve<PaymentsViewModel>());
+                    //    WindowService.Title = "Payments";
+                    //    break;
+                    //case DocumentType.PayMethods:
+                    //    WindowService.Show("PaymentMethod", scope.Resolve<PaymentMethodViewModel>());
+                    //    WindowService.Title = "Payment Methods";
+                    //    break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
